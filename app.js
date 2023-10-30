db2cbc19d344d068570d19554e060aa790109b4e
Saul <saulvargascosta@hotmail.com>
2023-10-30 00:33:22 -0300

    alteração cors

--- app.js
+++ app.js
@@ -12,11 +12,18 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+});
+
 const db = require("./db")
 
 app.use(express.json()) //configuração pra receber dados json
 
-app.get("/", ( req, res)=>{
+app.get("/", (req, res) => {
     console.log('Recebida solicitação GET para /usuarios');
     res.json({
         message: "Funcionanso, PUM!!!!!!!!!!!!!!!!!!!!!!!"
@@ -24,46 +31,38 @@ app.get("/", ( req, res)=>{
 })
 
 // Busca por id
-app.get("/usuario/:id", async (req, res)=>{
+app.get("/usuario/:id", async (req, res) => {
     const usuario = await db.selecionarUsuario(req.params.id)
-    res.json(usuario) 
+    res.json(usuario)
 })
 
 // Busca geral
-app.get("/usuarios", async (req, res)=>{ 
-    const usuarios = await db.selecionarUsuarios() 
+app.get("/usuarios", async (req, res) => {
+    const usuarios = await db.selecionarUsuarios()
     res.json(usuarios)
 })
 
 // Inserir POST
-app.post("/usuarios", async (req, res)=>{
+app.post("/usuarios", async (req, res) => {
     await db.inserirUsuario(req.body)
     res.sendStatus(201)
 })
 
 // Atualizar PATCH
-app.patch("/usuario/:id", async (req, res)=>{
-    await db.atualizarUsuario(req.params.id ,req.body)
+app.patch("/usuario/:id", async (req, res) => {
+    await db.atualizarUsuario(req.params.id, req.body)
     res.sendStatus(200)
 })
 
 // Deletar DELETE
-app.delete("/usuario/:id", async (req, res)=>{
+app.delete("/usuario/:id", async (req, res) => {
     await db.deletarUsuario(req.params.id)
     res.sendStatus(204)
 })
 
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', '*');
-
-    next();
-});
-
 app.use(cors());
-app.use(express.json()); 
-app.use(express.urlencoded({extended:true}));  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/personal", rotaPersonal);
 app.use("/api/treino", rotaTreino);
