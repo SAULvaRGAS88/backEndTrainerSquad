f1f79b3ab3df408a6ebec5eaf5355ad9cf76f80b
Sandro Teixeira <89029203+sandro1401@users.noreply.github.com>
2023-10-31 08:36:17 -0300

    alteracao bd online

--- controller/aluno_controller.js
+++ controller/aluno_controller.js
@@ -15,7 +15,7 @@ async function addAluno(req, res) {
         if (error.status) {
             res.status(error.status).json(error)
         } else {
-            res.status(402).json({message: "Aluno já cadastrado!"})
+            res.status(402).json({message: "Aluno já cadastrado Anteriormente!"})
         }
     }
 }
