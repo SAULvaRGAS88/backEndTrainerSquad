04e16b928622df29a36e15421bf61bbe733d732b
Saul <saulvargascosta@hotmail.com>
2023-11-01 00:54:11 -0300

    Alteração 2... excluindo porta 5000

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require("dotenv").config()
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const rotaPersonal = require('./rota/personal_rota')
 const rotaTreino = require('./rota/treino_rota')
