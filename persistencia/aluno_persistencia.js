486c64bdb0364ac6b2f8b7ba8d9cb3fd70d61a1e
Saul <saulvargascosta@hotmail.com>
2023-11-04 00:16:23 -0300

    alteração status pagamento

--- persistencia/aluno_persistencia.js
+++ persistencia/aluno_persistencia.js
@@ -13,22 +13,22 @@ async function addAluno(idUsuario, aluno) {
 
         const sql = `INSERT INTO aluno(sexo, nome, cpf, dt_nascimento, telefone, email, status, plano, idUsuario)
                                 VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id`
-        const values = [aluno.sexo, aluno.nome, aluno.cpf, aluno.dt_nascimento, aluno.telefone, aluno.email, "Ativo", 
-                        aluno.plano, idUsuario]       
-        resAluno = await client.query(sql, values)                         
+        const values = [aluno.sexo, aluno.nome, aluno.cpf, aluno.dt_nascimento, aluno.telefone, aluno.email, "Ativo",
+        aluno.plano, idUsuario]
+        resAluno = await client.query(sql, values)
 
         const sqlPag = `INSERT INTO pagamento(id_aluno, dt_pagamento, status, valor) VALUES($1, $2, $3, $4) RETURNING *`
-        const valuesPag = [resAluno.rows[0].id, aluno.pagamento.dt_pagamento, aluno.pagamento.status, aluno.pagamento.valor]
+        const valuesPag = [resAluno.rows[0].id, aluno.pagamento.dt_pagamento, "Pendente", aluno.pagamento.valor]
         const pag = await client.query(sqlPag, valuesPag)
 
         await client.query('COMMIT')
 
         return { aluno: resAluno.rows[0], pagamento: pag.rows[0] };
     } catch (error) {
-        await client.query('ROLLBACK'); 
+        await client.query('ROLLBACK');
         throw error;
     } finally {
-        client.end() 
+        client.end()
     }
 }
 
@@ -112,8 +112,8 @@ async function atualizarAluno(id, alunos) {
                                       status        = $7, 
                                       plano         = $8
                                 WHERE id = $9 RETURNING *`
-        const values = [alunos.sexo, alunos.nome, alunos.cpf, alunos.dt_nascimento, alunos.telefone, alunos.email, 
-                        alunos.status, alunos.plano, id]
+        const values = [alunos.sexo, alunos.nome, alunos.cpf, alunos.dt_nascimento, alunos.telefone, alunos.email,
+        alunos.status, alunos.plano, id]
         const alunoAtualizado = await client.query(sql, values)
 
         client.end()
@@ -139,17 +139,17 @@ async function deletarAluno(id) {
         const sqlAvaliacao = `DELETE FROM avaliacao WHERE idAluno = $1 RETURNING *`
         const valuesAvaliacao = [id]
         const avaliacao = await client.query(sqlAvaliacao, valuesAvaliacao)
-       
+
         const sql = `DELETE FROM aluno WHERE id = $1 RETURNING *`
         const values = [id]
         alunoDeletado = await client.query(sql, values)
 
         await client.query('COMMIT')
-        return {aluno: alunoDeletado.rows[0], pagamento: pag.rows[0], treino: treino.rows[0], avaliacao: avaliacao.rows[0]}
+        return { aluno: alunoDeletado.rows[0], pagamento: pag.rows[0], treino: treino.rows[0], avaliacao: avaliacao.rows[0] }
     } catch (error) {
-       
-        await client.query('ROLLBACK'); 
-        
+
+        await client.query('ROLLBACK');
+
         throw error;
     } finally { client.end() }
 }
@@ -165,5 +165,5 @@ module.exports = {
     buscarAlunoPorCpf,
     atualizarAluno,
     deletarAluno,
-   
+
 }
